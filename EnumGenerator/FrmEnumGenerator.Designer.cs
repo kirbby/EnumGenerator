e9ac69fdf50df28efc07a69e5daab7a1cba8b2cb
Matthias Winkler <github@m-winkler.at>
2019-03-09 19:57:03 +0100

    Initial commit

---
+++ EnumGenerator/FrmEnumGenerator.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace EnumGenerator
+{
+    partial class FrmEnumGenerator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtOutput = new System.Windows.Forms.RichTextBox();
+            this.BtnGenerate = new System.Windows.Forms.Button();
+            this.LblDatabase = new System.Windows.Forms.Label();
+            this.LblTable = new System.Windows.Forms.Label();
+            this.LblKey = new System.Windows.Forms.Label();
+            this.LblValue = new System.Windows.Forms.Label();
+            this.CboDatabase = new System.Windows.Forms.ComboBox();
+            this.CboTable = new System.Windows.Forms.ComboBox();
+            this.CboKey = new System.Windows.Forms.ComboBox();
+            this.CboValue = new System.Windows.Forms.ComboBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TxtOutput
+            // 
+            this.TxtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TxtOutput.Location = new System.Drawing.Point(12, 104);
+            this.TxtOutput.Name = "TxtOutput";
+            this.TxtOutput.ReadOnly = true;
+            this.TxtOutput.Size = new System.Drawing.Size(585, 459);
+            this.TxtOutput.TabIndex = 0;
+            this.TxtOutput.Text = "";
+            // 
+            // BtnGenerate
+            // 
+            this.BtnGenerate.Location = new System.Drawing.Point(12, 75);
+            this.BtnGenerate.Name = "BtnGenerate";
+            this.BtnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.BtnGenerate.TabIndex = 1;
+            this.BtnGenerate.Text = "Generate";
+            this.BtnGenerate.UseVisualStyleBackColor = true;
+            this.BtnGenerate.Click += new System.EventHandler(this.BtnGenerate_Click);
+            // 
+            // LblDatabase
+            // 
+            this.LblDatabase.AutoSize = true;
+            this.LblDatabase.Location = new System.Drawing.Point(3, 0);
+            this.LblDatabase.Name = "LblDatabase";
+            this.LblDatabase.Size = new System.Drawing.Size(84, 13);
+            this.LblDatabase.TabIndex = 6;
+            this.LblDatabase.Text = "Database Name";
+            // 
+            // LblTable
+            // 
+            this.LblTable.AutoSize = true;
+            this.LblTable.Location = new System.Drawing.Point(148, 0);
+            this.LblTable.Name = "LblTable";
+            this.LblTable.Size = new System.Drawing.Size(65, 13);
+            this.LblTable.TabIndex = 7;
+            this.LblTable.Text = "Table Name";
+            // 
+            // LblKey
+            // 
+            this.LblKey.AutoSize = true;
+            this.LblKey.Location = new System.Drawing.Point(293, 0);
+            this.LblKey.Name = "LblKey";
+            this.LblKey.Size = new System.Drawing.Size(56, 13);
+            this.LblKey.TabIndex = 8;
+            this.LblKey.Text = "ID Column";
+            // 
+            // LblValue
+            // 
+            this.LblValue.AutoSize = true;
+            this.LblValue.Location = new System.Drawing.Point(438, 0);
+            this.LblValue.Name = "LblValue";
+            this.LblValue.Size = new System.Drawing.Size(98, 13);
+            this.LblValue.TabIndex = 9;
+            this.LblValue.Text = "Description Column";
+            // 
+            // CboDatabase
+            // 
+            this.CboDatabase.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.CboDatabase.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.CboDatabase.FormattingEnabled = true;
+            this.CboDatabase.Location = new System.Drawing.Point(3, 23);
+            this.CboDatabase.Name = "CboDatabase";
+            this.CboDatabase.Size = new System.Drawing.Size(121, 21);
+            this.CboDatabase.TabIndex = 10;
+            this.CboDatabase.SelectedValueChanged += new System.EventHandler(this.CboDatabase_ValueChanged);
+            // 
+            // CboTable
+            // 
+            this.CboTable.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.CboTable.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.CboTable.FormattingEnabled = true;
+            this.CboTable.Location = new System.Drawing.Point(148, 23);
+            this.CboTable.Name = "CboTable";
+            this.CboTable.Size = new System.Drawing.Size(121, 21);
+            this.CboTable.TabIndex = 11;
+            this.CboTable.SelectedValueChanged += new System.EventHandler(this.CboTable_SelectedValueChanged);
+            // 
+            // CboKey
+            // 
+            this.CboKey.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.CboKey.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.CboKey.FormattingEnabled = true;
+            this.CboKey.Location = new System.Drawing.Point(293, 23);
+            this.CboKey.Name = "CboKey";
+            this.CboKey.Size = new System.Drawing.Size(121, 21);
+            this.CboKey.TabIndex = 12;
+            // 
+            // CboValue
+            // 
+            this.CboValue.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.CboValue.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.CboValue.FormattingEnabled = true;
+            this.CboValue.Location = new System.Drawing.Point(438, 23);
+            this.CboValue.Name = "CboValue";
+            this.CboValue.Size = new System.Drawing.Size(121, 21);
+            this.CboValue.TabIndex = 13;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.CboValue, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.LblDatabase, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.CboKey, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.LblTable, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.CboTable, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.LblKey, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.CboDatabase, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.LblValue, 3, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(583, 57);
+            this.tableLayoutPanel1.TabIndex = 14;
+            // 
+            // FrmEnumGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 575);
+            this.Controls.Add(this.BtnGenerate);
+            this.Controls.Add(this.TxtOutput);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "FrmEnumGenerator";
+            this.ShowIcon = false;
+            this.Text = "EnumGenerator";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox TxtOutput;
+        private System.Windows.Forms.Button BtnGenerate;
+        private System.Windows.Forms.Label LblDatabase;
+        private System.Windows.Forms.Label LblTable;
+        private System.Windows.Forms.Label LblKey;
+        private System.Windows.Forms.Label LblValue;
+        private System.Windows.Forms.ComboBox CboDatabase;
+        private System.Windows.Forms.ComboBox CboTable;
+        private System.Windows.Forms.ComboBox CboKey;
+        private System.Windows.Forms.ComboBox CboValue;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ EnumGenerator/FrmEnumGenerator.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace EnumGenerator
+{
+    public partial class FrmEnumGenerator : Form
+    {
+        private SqlConnection _Connection;
+        private List<string> _Columns;
+
+        public FrmEnumGenerator()
+        {
+            InitializeComponent();
+
+            InitCboDatabase();
+        }
+
+        private void InitCboDatabase()
+        {
+            _Connection = new SqlConnection(@"Server=.\SQLExpress; Integrated Security=true;");
+
+            CboDatabase.DataSource = GetListOnSelect("EXEC sp_databases", "DATABASE_NAME");
+        }
+
+        private void LoadCboTable()
+        {
+            CboTable.DataSource = GetListOnSelect("SELECT [TABLE_NAME] FROM [" + CboDatabase.SelectedValue + "].INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'", "TABLE_NAME");
+        }
+
+        private void LoadColumnCbos()
+        {
+            _Columns = GetListOnSelect("SELECT * FROM [" + CboDatabase.SelectedValue + "].INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '" + CboTable.SelectedValue + "'", "COLUMN_NAME");
+
+            var columnsKey = new List<string>();
+            var columnsValue = new List<string>();
+
+            columnsKey.AddRange(_Columns);
+            columnsValue.AddRange(_Columns);
+
+            CboKey.DataSource = columnsKey;
+            CboValue.DataSource = columnsValue;
+        }
+
+        private void BtnGenerate_Click(object sender, EventArgs e)
+        {
+            if (CboKey.SelectedValue == CboValue.SelectedValue)
+            {
+                MessageBox.Show("Please choose different columns for Key and Value!");
+                return;
+            }
+
+            if (string.IsNullOrEmpty((string)CboDatabase.SelectedValue) || string.IsNullOrEmpty((string)CboTable.SelectedValue) ||
+                string.IsNullOrEmpty((string)CboKey.SelectedValue) || string.IsNullOrEmpty((string)CboValue.SelectedValue))
+            {
+                MessageBox.Show("Please select something in every dropdown!");
+                return;
+            }
+
+            if (!IsDatatypesCorrect())
+            {
+                MessageBox.Show("The datatypes of the selected columns are not correct! ID = int; Value = string");
+                return;
+            }
+
+            TxtOutput.Text = "";
+            WriteOutput(
+                GetDictOnSelect("Select [" + CboValue.SelectedValue + "], [" + CboKey.SelectedValue + "] from [" + CboTable.SelectedValue + "]",
+                (string)CboKey.SelectedValue, (string)CboValue.SelectedValue)
+                );
+        }
+
+        private bool IsDatatypesCorrect()
+        {
+            var keyType = GetDatatype((string)CboKey.SelectedValue);
+            var valueType = GetDatatype((string)CboValue.SelectedValue);
+
+            if (keyType != "int" || !(valueType == "varchar" || valueType == "nchar" || valueType == "ntext" || valueType == "char" || valueType == "nvarchar" || valueType == "text"))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private void WriteOutput(Dictionary<int, string> dictionary)
+        {
+            TxtOutput.Text = "enum Permissions" + Environment.NewLine +
+                  "{" + Environment.NewLine;
+
+            foreach(var item in dictionary)
+            {
+                TxtOutput.Text += "\t" + item.Value + " = " + item.Key + "," + Environment.NewLine;
+            }
+
+            TxtOutput.Text += "}";
+
+            TxtOutput.Focus();
+
+            Clipboard.SetText(TxtOutput.Text);
+        }
+
+        private void CboDatabase_ValueChanged(object sender, EventArgs e)
+        {
+            _Connection = new SqlConnection(@"Server=.\SQLExpress; Integrated Security=true; Database = " + CboDatabase.SelectedValue + ";");
+
+            LoadCboTable();
+        }
+        
+        private void CboTable_SelectedValueChanged(object sender, EventArgs e)
+        {
+            LoadColumnCbos();
+        }
+
+        private List<string> GetListOnSelect(string select, string fieldName)
+        {
+            try
+            {
+                _Connection.Open();
+
+                var command = new SqlCommand(select, _Connection);
+                var databases = new List<string>();
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var item = reader[fieldName];
+
+                        if (!Convert.IsDBNull(item))
+                        {
+                            databases.Add((string)item);
+                        }
+                    }
+                }
+
+                _Connection.Close();
+
+                return databases;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to load data! " + Environment.NewLine + ex.Message);
+                _Connection.Close();
+
+                return null;
+            }
+        }
+
+        private Dictionary<int, string> GetDictOnSelect(string select, string fieldNameKey, string fieldNameValue)
+        {
+            try
+            {
+                _Connection.Open();
+
+                var command = new SqlCommand(select, _Connection);
+                var enumDict = new Dictionary<int, string>();
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var key = reader[fieldNameKey];
+                        var value = reader[fieldNameValue];
+
+                        if (!Convert.IsDBNull(key) && !Convert.IsDBNull(value))
+                        {
+                            enumDict.Add((int)key, (string)value);
+                        }
+                    }
+                }
+
+                _Connection.Close();
+
+                return enumDict;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to load data! " + Environment.NewLine + ex.Message);
+                _Connection.Close();
+
+                return null;
+            }
+        }
+
+        private string GetDatatype(string fieldName)
+        {
+            try
+            {
+                _Connection.Open();
+
+                var command = new SqlCommand("SELECT DATA_TYPE FROM [" + CboDatabase.SelectedValue + "].INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '" 
+                    + CboTable.SelectedValue + "' AND COLUMN_NAME = '" + fieldName + "'", _Connection);
+                var datatype = "";
+
+                using (var reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        var datatypeTemp = reader["DATA_TYPE"];
+
+                        if (!Convert.IsDBNull(datatypeTemp))
+                        {
+                            datatype = (string)datatypeTemp;
+                        }
+                    }
+                }
+
+                _Connection.Close();
+
+                return datatype;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to load data! " + Environment.NewLine + ex.Message);
+                _Connection.Close();
+
+                return null;
+            }
+        }
+    }
+}
